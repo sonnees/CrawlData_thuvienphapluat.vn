c2b38537ceb6572056f0641a4bc29ec91aa23f98
sonnees <sson12131415@gmail.com>
2024-03-05 07:29:16 +0700

    success crawl data: 10 page article 'bat dong san'

---
+++ CrawlData_thuvienphapluat/spiders/thuvienphapluat.py
@@ -0,0 +1,79 @@
+import scrapy
+from bs4 import BeautifulSoup
+import json
+
+def removeLinkTag(links):
+    for i in links:
+        if(i.startswith("/phap-luat/tag/")):
+            links.remove(i)
+    return links
+
+def cleanText(text):
+    text = text.strip()
+    text = text.replace("\n", "")
+    text = text.replace("\r", "")
+    return text
+
+class ThuvienphapluatSpider(scrapy.Spider):
+    name = "thuvienphapluat"
+    start_urls = ["https://thuvienphapluat.vn/phap-luat/bat-dong-san?page={}".format(i) for i in range(10)]
+    print("Start URL: ", start_urls)
+
+    def parse(self, response):
+        links = response.css('article a::attr("href")').getall()
+
+        links = removeLinkTag(links)
+
+        for link in links:
+            yield scrapy.Request(url="https://thuvienphapluat.vn"+link, callback=self.parse_detail_article)
+
+    def parse_detail_article(self, response):
+        url = response.request.url
+        html = response.text
+        soup = BeautifulSoup(html, features="lxml")
+        title = soup.find("h1").text
+        introduction = cleanText(soup.find("strong",{"class": "d-block mt-3 mb-3 sapo"}).text)
+        
+        title_content = soup.find_all("h2")
+        content = []
+        for index, h2_tag in enumerate(title_content):
+            siblings = h2_tag.find_next_siblings()
+
+            sub_content = []
+            for sibling in siblings:
+                if sibling.name == 'h2':
+                    break
+                if(sibling.name == 'p'):
+                    if(sibling.find("img")):
+                        sub_content.append(sibling.find("img")['src'])
+                    else: sub_content.append(cleanText(sibling.text))
+                if(sibling.name == 'blockquote'):
+                    from_law=[]
+                    ems = sibling.find_all("em")
+                    for em in ems:
+                        from_law.append(em.text)
+                    sub_content.append(from_law)
+                if(sibling.name == 'a'):
+                    continue
+
+            content.append({
+                "sub_title": h2_tag.find("strong").text,
+                "sub_content": sub_content
+            })
+        yield {
+            "url": url,
+            "title": str(title),
+            "introduction" : str(introduction),
+            "content" : content,
+        }
+
+        result =  {
+            "url": url,
+            "title": str(title),
+            "introduction" : str(introduction),
+            "content" : content,
+        }
+        
+        with open('output.json', 'a', encoding='utf-8') as json_file:
+            json.dump(result, json_file, ensure_ascii=False)
+            json_file.write('\n')  
